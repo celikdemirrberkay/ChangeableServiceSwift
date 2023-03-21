3d2b8c74e12e53248ff4119a3dd79e65265fa75d
celikdemirrberkay <berkaycelikdemirr@gmail.com>
2023-03-21 15:44:01 +0300

    Initial Commit

---
+++ ChangeableService/ChangeableServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeableServiceApp.swift
+//  ChangeableService
+//
+//  Created by Berkay  on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChangeableServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
